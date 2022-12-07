4e54cdde1f37d7554afa8d7058caf28b1dc69792
ahad-ullahm <ahadullahkhokhar@gmail.com>
2022-12-07 13:32:28 +0500

    Ye le haroon

--- LeftoverManagementApi/LeftoverManagementApi/Program.cs
+++ LeftoverManagementApi/LeftoverManagementApi/Program.cs
@@ -1,11 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using LeftoverManagementApi.Models;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//adding instance of application db context into the request pipeline and now we can get this instance throughout our request using dependency injection
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
