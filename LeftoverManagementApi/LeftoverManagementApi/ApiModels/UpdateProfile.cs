3b6a49457d33d59c9f9287d0c708721e985a0f13
ahad-ullah <ahadullahkhokhar@gmail.com>
2023-04-09 01:57:42 +0500

    updated updateProfile class

--- LeftoverManagementApi/LeftoverManagementApi/ApiModels/UpdateProfile.cs
+++ LeftoverManagementApi/LeftoverManagementApi/ApiModels/UpdateProfile.cs
@@ -2,7 +2,7 @@
 {
     public class UpdateProfile
     {
-        public IFormFile ImageFile { get; set; }
+        public IFormFile? ImageFile { get; set; }
         public string FullName { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
